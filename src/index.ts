11ac7fd040ca6ba0add9b88b6956fb0581745eda
DmShpak <dm.shpak.code@gmail.com>
2017-11-26 20:24:42 +0100

    [F] Export missing typescript interfaces

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@
 import { createCompiler } from './createCompiler'
 import { unar, binar, triar } from './tools'
 import { defaultOperationsMap } from './defaultOperatorsMap'
+import { Compiler, OperationsMap, TemplateObject } from './interfaces'
+
+export {
+    Compiler,
+    OperationsMap,
+    TemplateObject,
+}
 
 export {
     createCompiler,
